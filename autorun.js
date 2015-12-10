307df2909142addd89b984b1726dca4b856809d5
章瀚升 <Hans@Hans-MBA.local>
2015-12-10 12:49:36 +0800

    fix load panel

--- autorun.js
+++ autorun.js
@@ -19,7 +19,7 @@ if(!window.jQuery){
     f.setAttribute('src','https://hans00.github.io/lhu_labor/md5.min.js');
     document.getElementsByTagName('head')[0].appendChild(f);
 }
-if(!jQuery(document).has("#labor_panel")){
+if(jQuery(document).has("#labor_panel")==[]){
     $.get('https://hans00.github.io/lhu_labor/panel.html')
     .done(function (data) {
         //$("html").remove();
